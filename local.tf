6993953c5de6991963c9d297fb6d37f0309a3861
manoharmotam <b3stsinceday1@gmail.com>
2026-07-10 13:23:55 +0000

    Update: docker-instance config files

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-    ami = data.aws_ami.ami_id.id
+  ami    = data.aws_ami.ami_id.id
   vpc_id = data.aws_vpc.main.id
-  my_ip = ["${chomp(data.http.my_ip.response_body)}/32"]
+  my_ip  = ["${chomp(data.http.my_ip.response_body)}/32"]
 }
--- main.tf
+++ main.tf
@@ -1,56 +1,61 @@
 resource "aws_instance" "docker" {
-    ami = local.ami
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [ aws_security_group.docker_sg.id ]
-    user_data =  = templatefile("${path.module}/docker.sh.tftpl", {
-        partition_number = 4
-        extend_size = 30
-    })
+  ami                    = local.ami
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+  user_data = templatefile("${path.module}/docker.sh.tftpl", {
+    partition_number = 4
+    extend_size      = 30
+  })
 
-    root_block_device{
-        delete_on_termination = true
-        volume_size = 50
-        volume_type = "gp3"
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 50
+    volume_type           = "gp3"
 
-        tags = {
-        "Name" = "Docker-volume"
-        "Managed By" = "Terraform"
-        }
-    }
-    
     tags = {
-      "Name" = "Docker-instance"
+      "Name"       = "Docker-volume"
       "Managed By" = "Terraform"
     }
+  }
+
+  tags = {
+    "Name"       = "Docker-instance"
+    "Managed By" = "Terraform"
+  }
 }
 
 resource "aws_security_group" "docker_sg" {
-  name = "docker"
+  name   = "docker"
   vpc_id = local.vpc_id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "TCP"
-        cidr_blocks = local.my_ip
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = local.my_ip
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 90
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8000
+    to_port     = 9000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        ingress {
-        from_port = 8000
-        to_port = 9000
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-      "Name" = "Docker-instance"
-      "Managed By" = "Terraform"
-    }
+  tags = {
+    "Name"       = "Docker-instance"
+    "Managed By" = "Terraform"
+  }
 }
